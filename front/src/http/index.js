67cef7b89837dd3d63f5c3bc01f3bdd6d37f7e79
vit496.ru <a@vit496.ru>
2023-09-08 16:27:48 +0300

    a

--- front/src/http/index.js
+++ front/src/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const url = process.env.REACT_APP_API_URL || "http://localhost:3014";
-const url = "http://vit496.ru:30014";
+const url = process.env.REACT_APP_API_URL || "http://localhost:3014";
+//const url = "http://vit496.ru:30014";
 const $host = axios.create({
   // baseURL: process.env.REACT_APP_API_URL,
   // baseURL: "http://localhost:3014",
